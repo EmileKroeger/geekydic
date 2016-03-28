98f41f677b2052a5b090b54393d8642919d9f3d0
Emile Kroeger <flammifer@gmail.com>
2016-03-28 13:46:52 +0200

    improve display of JLPT level, with stars

--- app/scripts/controllers/skyrim.js
+++ app/scripts/controllers/skyrim.js
@@ -11,6 +11,22 @@ angular.module('geekydicApp')
   .controller('SkyrimCtrl', function ($scope, $http) {
     console.log("getting data..!");
     $scope.words = [];
+    $scope.stars = {
+      0: "",
+      1: "★",
+      2: "★★",
+      3: "★★★",
+      4: "★★★★",
+      5: "★★★★★",
+    }
+    $scope.nostars = {
+      0: "☆☆☆☆☆",
+      1: "☆☆☆☆",
+      2: "☆☆☆",
+      3: "☆☆",
+      4: "☆",
+      5: "",
+    }
     $http.get("data/skyrimvoc.json").then(function(words) {
       console.debug(words);
       $scope.words = words.data;
