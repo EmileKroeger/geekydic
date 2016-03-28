41bb3992c170191dbffb02d66150cab6260b15c6
Emile Kroeger <flammifer@gmail.com>
2016-03-28 13:04:00 +0200

    Fix a few bugs, add Skyrim data and a basic display of it

--- app/scripts/controllers/skyrim.js
+++ app/scripts/controllers/skyrim.js
@@ -8,7 +8,17 @@
  * Controller of the geekydicApp
  */
 angular.module('geekydicApp')
-  .controller('SkyrimCtrl', function ($scope) {
+  .controller('SkyrimCtrl', function ($scope, $http) {
+    console.log("getting data..!");
+    $scope.words = [];
+    $http.get("data/skyrimvoc.json").then(function(words) {
+      console.debug(words);
+      $scope.words = words.data;
+      //$scope.$apply(function() {
+      //  $scope.words = words;
+      //  console.log("got words");
+      //});
+    })
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
