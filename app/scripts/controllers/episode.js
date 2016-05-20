8a14826176ee4d8b9f16acb0ce9ad17c333152f7
Emile Kroeger <flammifer@gmail.com>
2016-05-20 13:59:43 +0200

    Add navigation links between episodes

--- app/scripts/controllers/episode.js
+++ app/scripts/controllers/episode.js
@@ -8,38 +8,33 @@
  * Controller of the geekydicApp
  */
 angular.module('geekydicApp')
-  .service('sEpisodeData', function ($http) {
+  .service('sEpisodeData', function (sDataCache) {
     this.get = function(source, episode, callback) {
       var dataFile = 'data/episodes/' + source + '_' + episode + '.json';
-      console.debug("Fetching: " + dataFile)
-      $http.get(dataFile).then(function(response) {
-        callback(response.data);
-      });
+      sDataCache.get(dataFile, callback);
     };
   })
-  .controller('EpisodeCtrl', function ($scope, $routeParams, sEpisodeData) {
+  .controller('EpisodeCtrl', 
+  function ($scope, $routeParams, sEpisodeData, sSeriesIndex) {
     $scope.source = $routeParams.source;
-    $scope.episode = $routeParams.episode;
+    $scope.episode = parseInt($routeParams.episode);
+    $scope.episodes = sSeriesIndex.episodes[$scope.source];
     $scope.vocabularyLists = null;
     $scope.vocDomains = [
       {
-        title: "Important words",
-        key: "important",
+        title: 'Important words',
+        key: 'important',
       },
       {
-        title: "Words you should already know",
-        key: "known",
+        title: 'Words you should already know',
+        key: 'known',
       },
       {
-        title: "Other words",
-        key: "other",
+        title: 'Other words',
+        key: 'other',
       },
-    ]
+    ];
     sEpisodeData.get($scope.source, $scope.episode, function(vocabularyLists) {
-      //$scope.$apply(function() {
-        console.log("I got data!");
-        $scope.vocabularyLists = vocabularyLists;
-        console.debug(vocabularyLists);
-        //});
+      $scope.vocabularyLists = vocabularyLists;
     });
   });
--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -8,11 +8,13 @@
  * Controller of the geekydicApp
  */
 angular.module('geekydicApp')
-  .controller('HomeCtrl', function ($scope) {
-    $scope.series = [
-      {
-        "name": "naruto",
-        "episodes": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-      },
-    ];
+  .service('sSeriesIndex', function() {
+    this.episodes = {
+      naruto: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    }
+    this.series = ['naruto'];
+  })
+  .controller('HomeCtrl', function ($scope, sSeriesIndex) {
+    $scope.series = sSeriesIndex.series;
+    $scope.episodes = sSeriesIndex.episodes;
   });
